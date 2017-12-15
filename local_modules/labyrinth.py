ef11e45b29e68538319164c57854e480e085b3f5
JBthePenguin <jblab976@gmail.com>
2017-12-15 16:35:16 +0300

    display of the labyrinth on the gui

--- local_modules/labyrinth.py
+++ local_modules/labyrinth.py
@@ -1,25 +1,71 @@
-import os
-
 """ Module with the class Labyrinth"""
 
+import os
+import pygame
+
+main_dir = os.path.dirname(os.path.dirname(__file__))
+
+
 class Labyrinth():
-	"""docstring for Labyrinth
-	__init__()"""
-	def __init__(self):
-		""" Function : initialise an object with attribute zones {}"""
-		self.zones = {}
-		main_directory = os.path.dirname(os.path.dirname(__file__)) # we get the right path...
-		path_to_file = os.path.join(main_directory, "maps", "default_map.txt")
-		with open(path_to_file, "r") as f: # ...and we open the file
-			x = 0
-			y = 0
-			for elt in f.read():
-				if elt == "\n":
-					x = 0
-					y += 1
-				else:
-					if elt == "O":
-						self.zones[(x,y)] = "wall"
-					else:
-						self.zones[(x,y)] = "background"
-					x += 1
+    """docstring for Labyrinth : Class with methods
+    -   __init__()
+    -   display"""
+
+    def __init__(self):
+        """ Function : initialise object with attribute
+        -- background = {position xy : path towards the corresponding img}"""
+        self.background = {}
+        # get the right path
+        path_to_file = os.path.join(main_dir, "maps", "default_map.txt")
+        # open the file
+        with open(path_to_file, "r") as f:
+            # Initialize the position
+            x = 0
+            y = 0
+            # read character by character
+            for elt in f.read():
+                if elt == "\n":
+                    # update position
+                    x = 0
+                    y += 1
+                else:
+                    # associate corresponding image
+                    if elt == "O":
+                        path_to_img = os.path.join(main_dir, "img", "wall.png")
+                    else:
+                        path_to_img = os.path.join(
+                            main_dir, "img", "floor.png")
+                    # add key (x,y) and value (path to the file) in background
+                    self.background[(x, y)] = path_to_img
+                    # update position
+                    x += 1
+
+    def display(self):
+        """ Function : display the background
+        of the labyrinth on  a gui """
+        pygame.init()
+        # size of gui's window = 600 x 600 pixels
+        window = pygame.display.set_mode((600, 600))
+        # Initialise the position
+        x = 0
+        y = 0
+        while y < 15:
+            # take the right image
+            img = pygame.image.load(self.background[(x, y)])
+            # stick on the window
+            window.blit(img, ((x * 40), (y * 40)))
+            # update the position
+            if x < 14:
+                x += 1
+            else:
+                x = 0
+                y += 1
+        # refresh the display
+        pygame.display.flip()
+        # keep the display on ...
+        gui_display = 1
+        while gui_display:
+            for event in pygame.event.get():
+                # ... unless the window is closed
+                if event.type == pygame.QUIT:
+                    gui_display = None
--- macescapes.py
+++ macescapes.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-#from modules.labyrinth import Labyrinth
-#from modules import labyrinth
+# from modules.labyrinth import Labyrinth
+# from modules import labyrinth
 import local_modules.labyrinth as labyrinth
 
+
 def main():
-	""" main function """
-	gaming_map = labyrinth.Labyrinth()
-	print(gaming_map.zones)
+    """ main function """
+    gaming_map = labyrinth.Labyrinth()
+    gaming_map.display()
+
 
 if __name__ == "__main__":
-	main()
+    main()
